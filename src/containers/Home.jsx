0969518e74347cfee3f8c6b68d9dfa96df0b420c
Zachary Lim <zachary.lim@hotmail.com>
2018-05-24 21:29:54 -0700

    add rough home hero

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import homeHero from '../asset/img/home-hero.png';
 
 class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <div className="home">
+        <section className="home-hero">
+          <img src={homeHero} alt=""/>
+          <div className="hero-content">
+             <h1 className="title">Unlock the Limitless Potential</h1>
+              <div className="subtitle">Only the Outstanding blockchain developers. On demand.</div>
+              <form>
+                <input type="text" name="search"/>
+                <button>Search</button>
+              </form>
+          </div>
+        </section>
+        <section className="home-body"></section>
+      </div>
     );
   }
 }
